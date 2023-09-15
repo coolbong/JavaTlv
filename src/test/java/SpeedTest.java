803f46450d3cd665eba09605780d0e5eee5339e0
coolbong <coolbong.works@gmail.com>
2023-09-15 15:11:13 +0900

    add more hex method

--- src/test/java/SpeedTest.java
+++ src/test/java/SpeedTest.java
@@ -120,6 +120,24 @@ public class SpeedTest {
     }
 
     @Test
+    public void speed_test_006() {
+        byte[] arr = {
+                (byte) 0x40, (byte) 0x41, (byte) 0x42, (byte) 0x43, (byte) 0x44, (byte) 0x45, (byte) 0x46, (byte) 0x47,
+                (byte) 0x48, (byte) 0x49, (byte) 0x4a, (byte) 0x4b, (byte) 0x4c, (byte) 0x4d, (byte) 0x4e, (byte) 0x4f
+        };
+        //long start = System.currentTimeMillis();
+        Instant beforeTime = Instant.now();  // 코드 실행 전에 시간 받아오기
+        for (int i=0; i<100000; i++){
+            Hex.toHex4(arr);
+        }
+        Instant afterTime = Instant.now();
+        long diffTime = Duration.between(beforeTime, afterTime).toMillis(); // 두 개의 실행 시간
+        System.out.println("실행 시간(ms): " + diffTime);
+        //long duration = System.currentTimeMillis() - start;
+        //System.out.println("speed_test_001: " + duration + "ms");
+    }
+
+    @Test
     public void speed_tlv_parse_001() {
         String resp = "6F20840E315041592E5359532E4444463031A50E5F2D046B6F656E9F110101880101";
         Instant beforeTime = Instant.now();  // 코드 실행 전에 시간 받아오기
