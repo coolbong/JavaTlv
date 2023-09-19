caed7137c2cdd2d0ce1dd946f4a3a8c75a7fe60c
coolbong <coolbong.works@gmail.com>
2023-09-19 14:05:20 +0900

    change data

--- src/test/java/AsciiTest.java
+++ src/test/java/AsciiTest.java
@@ -9,8 +9,8 @@ import static org.junit.Assert.assertEquals;
 public class AsciiTest {
     @Test
     public void test_to_ascii_001() {
-        String hex = "4B4C5343204455414C20435245444954"; // KLSC DUAL CREDIT
-        String answer = "KLSC DUAL CREDIT";
+        String hex = "435245444954"; // KLSC DUAL CREDIT
+        String answer = "CREDIT";
 
         String ret = Hex.toAscii(hex);
 
